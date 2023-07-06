e3a445343de2f0e9a9100978fc83f3b98bb19db2
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-07-05 21:05:23 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// import dependencies
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+
+// express application
+const app = express();
+
+// middleware
+app.use(morgan('dev')); // log requests to the console
+app.use(methodOverride('_method')); //override with post having ?_method=DELETE or ?_method=put
+app.use(express.static('public')); // serve static files from the public folder
+
+// Routes
+
+app.get('/' , (req, res) => {
+    res.send("hello world")
+})
+
+
+// listen 
+const PORT = process.env.PORT || 3000;
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`listening on port ${PORT}`);
+});
